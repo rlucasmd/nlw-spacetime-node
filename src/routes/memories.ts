ac3c7da022ab2738c7304e48c5ddcba8d7837995
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-18 12:56:18 -0300

    add: list memories / get specific memory / create a new memory

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
+import { z } from "zod";
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get("/memories", async () => {
@@ -16,8 +17,38 @@ export async function memoriesRoutes(app: FastifyInstance) {
       };
     });
   });
-  // app.get("/memories/:id", async () => {});
-  // app.post("/memories", async () => {});
+  app.get("/memories/:id", async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = paramsSchema.parse(request.params);
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+
+    return memory;
+  });
+  app.post("/memories", async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    });
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body);
+    console.log(content, isPublic, coverUrl);
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        isPublic,
+        coverUrl,
+        userId: "c9867289-6db1-4b60-bfa1-9a04ff8bfc4e",
+      },
+    });
+
+    return memory;
+  });
   // app.put("/memories/:id", async () => {});
   // app.delete("/memories/:id", async () => {});
 }
